3ebda6c7a84c17c553b351607f1e536d962dd0e8
Rosie Burkett <burkettroz@gmail.com>
2018-02-07 12:00:04 +1300

    build initial grid

--- minesweeper.js
+++ minesweeper.js
@@ -1,7 +1,14 @@
 document.addEventListener('DOMContentLoaded', startGame)
 
 // Define your `board` object here!
-// var board = 
+var board = {
+  cells: [
+    {row: 0, col: 0, isMine: '', hidden: 'true'}, 
+    {row: 0, col: 1, isMine: '', hidden: 'true'},
+    {row: 1, col: 0, isMine: '', hidden: 'true'}, 
+    {row: 1, col: 1, isMine: '', hidden: 'true'},
+  ]
+}
 
 function startGame () {
   // Don't remove this function call: it makes the game work!
